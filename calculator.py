fc331d597ec8eddab0519bc2c41082eb0818dffe
j7r.cod3 <143880965+J73809@users.noreply.github.com>
2024-09-02 22:01:53 +0200

    Update

---
+++ calculator.py
@@ -0,0 +1,12 @@
+operator = input('Chose operation (+,-,*,/)')
+nr1 = int(input('Enter the 1st nr'))
+nr2 = int(input('Enter the 2nd nr'))
+
+if operator == '+':
+    print(nr2+nr2)
+elif operator == '-':
+    print(nr1-nr2)
+elif operator == '*':
+    print(nr1*nr2)
+elif operator == '/':
+    print(nr1/nr2)
